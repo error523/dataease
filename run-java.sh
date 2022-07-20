ec68b3527015f4ead3983889612f0d34063d0259
tdgp <puhaoqiang@tdgcore.com>
2022-07-20 17:25:04 +0800

    fix path

--- run-java.sh
+++ run-java.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar -DrootPath="/opt/dataease/" -DrootLogFile="/opt/dataease/dataease.properties" -Dspring.profiles.active=whole /opt/dataease/backend-${APP_VERSION}.jar
+java -jar -DrootPath="/opt/dataease/" -DrootLogFile="/opt/dataease/dataease.properties" -Dspring.profiles.active=whole /opt/apps/backend-${APP_VERSION}.jar
