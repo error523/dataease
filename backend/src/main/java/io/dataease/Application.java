d27616f6493c84365616789a0809856d71024381
tdgp <puhaoqiang@tdgcore.com>
2022-07-21 17:14:30 +0800

    fix path

--- backend/src/main/java/io/dataease/Application.java
+++ backend/src/main/java/io/dataease/Application.java
@@ -14,7 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 })
 @ServletComponentScan
 @EnableScheduling
-@PropertySource(value = "file:/${rootPath}/dataease.properties", encoding = "UTF-8", ignoreResourceNotFound = true)
+@PropertySource(value = "file://${rootPath}/dataease.properties", encoding = "UTF-8", ignoreResourceNotFound = true)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
